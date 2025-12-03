8bc4551a576886922d3ffae1ff0d83548b15ca71
shamim2011 <akhtarmdsamim786@gmail.com>
2025-12-03 12:19:43 +0530

    Some short cut in 10 days refactor

--- src/main/java/com/mycomp/firstApp/controller/MyController.java
+++ src/main/java/com/mycomp/firstApp/controller/MyController.java
@@ -2,6 +2,8 @@ package com.mycomp.firstApp.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,11 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mycomp.firstApp.pojo.NumReq;
 
 @RestController
@@ -24,21 +24,46 @@ public class MyController {
 	
 	@GetMapping
 	public String getMethod() {
-		return "Hello Spring getMethod!";
+		return "/index.html";
 	}
 	@PostMapping("/postm/{path1}/{path2}/process")
-	public String postMethod(@RequestParam("val1") int val1,
-							@RequestParam("val1") int val2,
-							@PathVariable String path1,
-							@PathVariable String path2,
-							@RequestHeader String myHeader,
-							@RequestBody NumReq reqBody) {
+	@ResponseStatus(HttpStatus.CREATED)        // Status will be 201
+	public ResponseEntity<String> postMethod(@RequestParam("val1") int val1,
+			@RequestParam("val1") int val2,
+			@PathVariable String path1,
+			@PathVariable String path2,
+			@RequestHeader String myHeader,
+			@RequestBody NumReq reqBody) {
+
+//		logger.info("NumRequest:{}",reqBody);
 		
-		logger.info("NumRequest:{}",reqBody);
+		convertStringToObject(reqBody);
 		
-		return "Hello Spring postMethod()!"+" \nval1="+val1+
-				" \nval2="+val2+"\nPath1="+path1+"\nPath2="+path2
-				+"\nmyHeader="+myHeader+
-				"\n|numReq = "+reqBody;
+		String responseBody = "Hello Spring postMethod()!"+" \nval1="+val1+
+		" \nval2="+val2+"\nPath1="+path1+"\nPath2="+path2
+		+"\nmyHeader="+myHeader+
+		"\n|numReq = "+reqBody;
+		
+		// TODO
+		return null; //responseBody;
 	}
+	private void convertStringToObject(NumReq reqBody) {
+		logger.info("NumRequest:{}",reqBody);
+	}
+	
+	
+//	public String postMethod(@RequestParam("val1") int val1,
+//							@RequestParam("val1") int val2,
+//							@PathVariable String path1,
+//							@PathVariable String path2,
+//							@RequestHeader String myHeader,
+//							@RequestBody NumReq reqBody) {
+//		
+//		logger.info("NumRequest:{}",reqBody);
+//		
+//		return "Hello Spring postMethod()!"+" \nval1="+val1+
+//				" \nval2="+val2+"\nPath1="+path1+"\nPath2="+path2
+//				+"\nmyHeader="+myHeader+
+//				"\n|numReq = "+reqBody;
+//	}
 }
